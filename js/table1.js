39541c2501ced798b727a87dc926c87f196d27a1
GregoryDi <slysuslik@mail.tu>
2017-07-10 22:12:03 +0300

    second commit

--- js/table1.js
+++ js/table1.js
@@ -1,4 +1,5 @@
 var Arrayforders=new Array;
+var arr = new Array;
 var oldcount=0;
 var color = getRandomColor();
 var n=1;
@@ -12,11 +13,15 @@ var trow=0;
 var oldn=289;
 
 
+
+var f=1;
+
+
 function ShowMatrix() {
       var table = document.createDocumentFragment();
       var n=1;
       var arr = [];
-      var x=n;
+
       for (var i = 0; i < 289; i++) {
           var tr = document.createElement('tr');
           arr[i] = [];
@@ -24,14 +29,19 @@ function ShowMatrix() {
 
           for(var j = 0; j < 8; j++) {
               var td = document.createElement('td');
+
+
               if (i==0){td.innerHTML = arr[i][j]=j;
-                  tr.appendChild(td);}
+                  tr.appendChild(td);
+                td.classList.add("hours");}
               if (j==0){
                  if (i == n){
                      n=n+12;
                      td.rowSpan = "12";
                      td.innerHTML = arr[i][j]=parseInt(n/12);
                      tr.appendChild(td);
+                     td.classList.add("hours");
+
                      }
               }
               else  {
@@ -45,7 +55,7 @@ function ShowMatrix() {
 
       }
       document.getElementById('matrix').appendChild(table);
-      console.log(x.time);
+
 }
 
 
@@ -65,10 +75,14 @@ function readFile() {
 
 
 function parseContent(content) {
+var y = new Array
+ arr=content.split(/\r?\n\s*\s*/);
+ y = arr.join(' ');
+ arr = y.split(' ');
 
-  t=Number(content.replace(/\D+/g,""));
-  alert(t);
-  return t;
+ console.log(arr);
+
+  return arr;
    }
 
 function getRandomColor() {
@@ -80,10 +94,13 @@ function getRandomColor() {
      return color;
 }
 
-function PasteOrder(t){
+function PasteOrder(arr){
+  for(var i=1; i <arr.length; i+=2){
+    var t = arr[i];
    Arrayforders.push(t);
-   console.log(Arrayforders);
    ShowOrders(Arrayforders);
+ }
+ console.log(Arrayforders);
 }
 
 
@@ -101,7 +118,9 @@ function ShowOrders(Arrayforders){
           else z=0;
           if(n==oldn){tcol++;trow+=288;oldn=n+288;}
 
-          elem =document.childNodes[1].childNodes[2].childNodes[3].childNodes[1].childNodes[1].childNodes[n-trow].childNodes[tcol+z];
+          elem =document.childNodes[1].childNodes[2].childNodes[3].childNodes[1].childNodes[n-trow].childNodes[tcol+z];
+          console.log("n-trow"+ n, trow);
+            console.log("tcol+z"+ tcol, z);
           elem.style.backgroundColor = color;
           console.log(elem);
           console.log(n);
@@ -109,10 +128,72 @@ function ShowOrders(Arrayforders){
           console.log(x);
           check=false;
           oldcount=count;
+          $(elem).attr("id","Cell"+n);
+          $(elem).attr("Class","Cellclass"+q);
+          $(elem).css({ border: 0 + "px" });
+
+
+
+
+          cell =  document.getElementsByClassName('Cellclass'+q);
+          countofclass = document.getElementsByClassName('Cellclass'+q).length
+        //  console.log(countofclass);
+        //  console.log(cell);
         }
 
     }
-
-
+  //  $( cell ).wrap( "<div id = 'order'></div>" );
+    // document.getElementById("order").addEventListener("mousedown", mouseDown);
 
    }
+
+
+function mouseDown() {
+
+         // for (f;f<countofclass+1;f++){
+
+            //cell = document.getElementById("Cell"+f);
+
+         // }
+
+            cell = document.getElementById("order");
+            cell.onmousedown = function(event) {
+            function getCoords(cell) {
+                var box = cell.getBoundingClientRect();
+                   return {
+                   top: box.top + pageYOffset,
+                   left: box.left + pageXOffset  };
+                   }
+            var coords = getCoords(cell);
+            var shiftX = event.pageX - coords.left;
+            var shiftY = event.pageY - coords.top;
+            document.body.appendChild(cell);
+            moveAt(event);
+            cell.style.zIndex = 1000;
+
+               function moveAt(event) {
+               cell.style.left= event.pageX - shiftX + 'px';
+               cell.style.top = event.pageY - shiftY + 'px';
+             }
+
+
+               document.onmousemove = function(event) {
+                   moveAt(event);
+                 };
+
+                   cell.onmouseup = function() {
+                   document.onmousemove = null;
+                   cell.onmouseup = null;
+                 };
+
+               }
+
+               cell.ondragstart = function() {
+                 return false;
+               };
+
+            console.log(f);
+
+
+
+          }
